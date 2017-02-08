626cb49900c23a462010a949fa13eb6d3e6fe3f3
Salih Kardan <salih@opsgenie.com>
2017-02-08 23:03:25 +0300

    ttest

---
+++ src/test/java/me/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package me;
+
+import org.junit.Test;
+
+/**
+ * @author Salih Kardan
+ * @version 08/02/2017.
+ */
+public class HelloControllerTest {
+
+    @Test
+    public void testSomething(){
+        System.out.println("stupid test.");
+    }
+}
